40e2bcc215d41fb1f5b683b54f1c3bedeb0c92b6
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-03-17 02:34:52 +0100

    fixed the bug on gif...

--- image_slider.py
+++ image_slider.py
@@ -1,42 +1,44 @@
+''' Importing the neccessary modules '''
 from itertools import cycle
 import tkinter as tk
 
 
 class App(tk.Tk):
-    # 
-    def __init__(self, image_files, x,y, delay):
+    # Tk window/Label adjusts to size of image
+    def __init__(self, image_files, x, y, delay):
+
         tk.Tk.__init__(self)
+
         self.geometry('+{}+{}'.format(x,y))
         self.delay = delay
+
         self.pictures = cycle((tk.PhotoImage(file=image), image) 
         for image in image_files)
         self.pictures_display = tk.Label(self)
         self.pictures_display.pack()
     
     def show_slides(self):
+
+        # Cycle through the images and displays them
         img_object, img_name = next(self.pictures)
         self.pictures_display.config(image = img_object)
+
         self.title(img_name)
-        self.afte(self.delay,self.show_slides)
+        self.after(self.delay, self.show_slides)
 
     def run(self):
         self.mainloop()
-        
+
+# set milliseconds time between slides        
 delay = 3500
 
 # Image gotten from unsplash.com
-image_files = [
-    '1.jpg',
-    '2.jpg',
-    '3.jpg',
-    '4.jpg',
-    '5.jpg',
-    '6.jpg',
-    '7.jpg', ]
+
+image_files = ['1.gif','2.gif','3.gif','4.gif','5.gif','6.gif','7.gif']
 
 x = 100
 y = 50
 
-imgApp = App(image_files,x,y,delay)
+imgApp = App(image_files, x, y, delay)
 imgApp.show_slides()
 imgApp.run()
